77dc12fa7273dd498f5e3080938120996d490bcb
Sam Kolb <skolb@creativechannel.com>
2013-04-07 10:41:43 -0700

    moved the directory to root

---
+++ MonkeyMayhem/MonkeyMayhem/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MonkeyMayhem
+//
+//  Created by skolb on 4/7/13.
+//  Copyright Bony Bois 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
